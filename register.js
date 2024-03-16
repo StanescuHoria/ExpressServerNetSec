a8d1de3746828f3fafdd6c413522efc7fd434249
Stanescu Horia <horiabvandrei@yahoo.com>
2024-03-16 09:57:13 -0400

    second-commit

--- register.js
+++ register.js
@@ -4,7 +4,24 @@ const argon2 = require('argon2');
 function registerUser(username, password) {
   const db = new sqlite3.Database('./users.db');
   
-  argon2.hash(password)
+  const fastOptions = {
+    type: argon2.argon2id,
+    timeCost: 2,
+    memoryCost: 4096,
+    parallelism: 1
+  };
+  
+  // Slow setup: Higher cost parameters
+  const slowOptions = {
+    type: argon2.argon2id,
+    timeCost: 4,
+    memoryCost: 65536,
+    parallelism: 2
+  };
+  
+  const options = fast ? fastOptions : slowOptions;
+
+  argon2.hash(password, options)
     .then(hash => {
       const sql = `INSERT INTO users (username, password) VALUES (?, ?)`;
       db.run(sql, [username, hash], function(err) {
@@ -24,10 +41,11 @@ function registerUser(username, password) {
 }
 
 // Usage: node register.js <username> <password>
-if (process.argv.length !== 4) {
-  console.log('Usage: node register.js <username> <password>');
+if (process.argv.length < 4 || process.argv.lenght > 5) {
+  console.log('Usage: node register.js <username> <password> [fast|slow]');
   process.exit(1);
 }
 
 const [,, username, password] = process.argv;
-registerUser(username, password);
+const fast = process.argv.length === 4 || process.argv[4] === 'fast';
+registerUser(username, password, fast);
